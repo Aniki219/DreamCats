b80d39ce54e4589a55090628e118818d0f478a4b
aniki219 <aniki219@gmail.com>
2024-04-21 16:01:08 -0700

    hi test

--- src/app/daycare/page.tsx
+++ src/app/daycare/page.tsx
@@ -3,43 +3,44 @@ import { CatData } from "@/models/Cat";
 import axios from "axios";
 import getUrl from "../lib/utils/getUrl";
 
-const getCats = async () => {
-    const {data} = await axios.get(getUrl(`/api/cats`), {method:"GET"});
-    return data;
-}
+// const getCats = async () => {
+//     const {data} = await axios.get(getUrl(`/api/cats`), {method:"GET"});
+//     return data;
+// }
 
 export default async function DayCare() {
-    const {cats} = await getCats();
+    return <h1>hi</h1>
+    // const {cats} = await getCats();
 
-    const catsList = () => {
-        if (!cats) {
-            return (
-                <p>
-                    Fetching Cats List...
-                </p>
-            )
-        } else {
-            console.log(cats);
-            return (
-                <ul>
-                    {cats.map((cat:CatData, i:number) => (
-                        <li key={i}>
-                            <button>{cat.name}</button>
-                        </li>
-                    ))}
-                </ul>
-            )
-        }
-    }
+    // const catsList = () => {
+    //     if (!cats) {
+    //         return (
+    //             <p>
+    //                 Fetching Cats List...
+    //             </p>
+    //         )
+    //     } else {
+    //         console.log(cats);
+    //         return (
+    //             <ul>
+    //                 {cats.map((cat:CatData, i:number) => (
+    //                     <li key={i}>
+    //                         <button>{cat.name}</button>
+    //                     </li>
+    //                 ))}
+    //             </ul>
+    //         )
+    //     }
+    // }
     
-    return (
-        <div>
-            <h2>Daycare</h2>
-            <p>Select a Cat to adopt</p>
-            {catsList()}
-            <p>Choose one</p>
+    // return (
+    //     <div>
+    //         <h2>Daycare</h2>
+    //         <p>Select a Cat to adopt</p>
+    //         {catsList()}
+    //         <p>Choose one</p>
 
-            <Link href="./tree">Return to the Tree</Link>
-        </div>
-    )
+    //         <Link href="./tree">Return to the Tree</Link>
+    //     </div>
+    // )
 }
