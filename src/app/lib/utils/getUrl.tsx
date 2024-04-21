694eb0a9a2ed0b4747658a122b482d38e748cd72
aniki219 <aniki219@gmail.com>
2024-04-21 13:53:50 -0700

    blank

--- src/app/lib/utils/getUrl.tsx
+++ src/app/lib/utils/getUrl.tsx
@@ -1,7 +1,7 @@
 const BASE_URL =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    : ``;
 
 const INCLUDES_FORWARD_SLASH_AT_START_REGEX = /^\/(.|\n)*$/;
 const INCLUDES_FORWARD_SLASH_AT_START = (string: string) =>
