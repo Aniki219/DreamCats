863e38ed0cf3c26894136c86b4f05f0a1b179941
aniki219 <aniki219@gmail.com>
2024-04-27 00:06:24 -0700

    secret no longer public

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -21,7 +21,7 @@ export const authConfig = {
     session: {
       strategy: "jwt"
     },
-    secret: process.env.NEXT_PUBLIC_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
     debug: process.env.NODE_ENV === "development",
     providers: [
         Credentials({
